34d95f826b3b6d916fa701a0061175a3462d1e48
mdhyduu <mdhhltstwr86@gmal.com>
2025-08-28 18:35:59 +0300

    وصف التحديثات

--- app/orders.py
+++ app/orders.py
@@ -513,12 +513,18 @@ def index():
             if order_type in ['all', 'salla']:
                 # استخدام join للتصفية حسب slug من جدول OrderStatus
                 salla_query = salla_query.join(SallaOrder.status).filter(OrderStatus.slug == status_filter)
-            
-            if order_type in ['all', 'custom']:
+
+             if order_type in ['all', 'custom']:
+                # لو الفلتر يرسل ID
                 custom_query = custom_query.filter(
-                    CustomOrder.status.has(EmployeeCustomStatus.slug == status_filter)
-    )                    
-        # تطبيق فلتر الموظف
+                    CustomOrder.status.has(EmployeeCustomStatus.id == status_filter)
+                )
+            
+                # أو لو الفلتر يرسل الاسم
+                # custom_query = custom_query.filter(
+                #     CustomOrder.status.has(EmployeeCustomStatus.name == status_filter)
+                # )                   
+                    # تطبيق فلتر الموظف
         if employee_filter:
             if order_type in ['all', 'salla']:
                 salla_query = salla_query.join(OrderAssignment).filter(OrderAssignment.employee_id == employee_filter)
