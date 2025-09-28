cc6611178957a8cc1e01e859b8b96641e5b63a1c
mdhyduu <mdhhltstwr86@gmal.com>
2025-09-28 06:54:35 +0300

    وصف التحديثات

--- app/utils.py
+++ app/utils.py
@@ -164,7 +164,7 @@ def generate_barcode(data):
             writer.set_options({
                 'write_text': True,
                 'module_width': 0.6,  # تقليل العرض قليلاً
-                'module_height': 25,   # تقليل الارتفاع قليلاً
+                'module_height': 15,   # تقليل الارتفاع قليلاً
                 'quiet_zone': 3,      # منطقة هادئة أصغر
                 'font_size': 8,       # حجم خط أصغر
                 'text_distance': 3,   # تقليل المسافة بين النص والباركود
@@ -217,7 +217,7 @@ def generate_barcode_with_code39(data):
         writer.set_options({
             'write_text': True,
             'module_width': 0.6,
-            'module_height': 25,
+            'module_height': 15,
             'quiet_zone': 3,
             'font_size': 10,
             'text_distance': 5,
@@ -254,7 +254,7 @@ def generate_barcode_alternative(data):
             'text': data,
             'quiet_zone': 3,
             'module_width': 0.6,
-            'module_height': 25,
+            'module_height': 15,
             'font_size': 9,
             'text_distance': 2
         })
